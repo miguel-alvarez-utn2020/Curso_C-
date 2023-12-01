2b5411c138c66f89e2578d4fa547894819e7692b
Miguel <miguealvarez48@gmail.com>
2023-12-01 17:22:10 -0300

    primer commit

---
+++ MiPrimerApp/MiPrimerApp/Program.cs
@@ -0,0 +1,39 @@
+﻿7777777777777777777777777777777779999999999999999999999999999999999999999999999999999+*-++++++666666666using System;9
+using System.Collections.Generic;0.
+    0
+    +
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiPrimerApp// un namespace, es un contenedor que usa .net organizar y almacenar clases
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                bloque de comentario ctrl + k ctrl + c
+             */
+            //tipos de variables
+            int varUno = 1, varDos = 2, varTres = 3;//declarar multiples en una sola linea
+            int numberInt = 10;
+            double numberDouble = 10.5;
+            float numebrFloat = 30.5f;
+            bool isValid = false;//true
+            char myChar = 'K';
+            string saludar = "Hola a todos";
+            decimal promedio = 8.5M;
+
+            //mostrar mensajes en consola.
+
+            Console.WriteLine($"Hola mundo {varUno} otro valor {varTres}");
+            Console.WriteLine("Hola mundo {0} otra variable {1}", varUno, varDos);
+            Console.WriteLine("Hola mundo " + varUno + " mas texto " + varTres);
+            Console.WriteLine(varTres);
+            +66666666666666789
+
+        }
+
+    }
+}
